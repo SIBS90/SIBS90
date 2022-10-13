481a2f6e812e76ca02b0848cd32b1af2baec622b
Sibs Ryan <joelryan178@gmail.com>
2022-10-13 09:38:50 +0100

    User_account

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Authentification : Form
+    {
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "admin" && textBox2.Text == "admin")
+            {
+                User_account user = new User_account();
+                user.Show();
+                this.Hide();
+            }
+            else
+            {
+               // MessageBox.Show("erreur", "erreur", MessageBoxIcon.Error);
+            }
+        }
+    }
+}
